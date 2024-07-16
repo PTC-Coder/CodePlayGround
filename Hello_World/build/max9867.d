144fd2a97e49ef057c03def541fea540d5c1b8c4
Patrick C <patrick@chaopricha.com>
2024-07-15 20:41:58 -0400

    Commit Hello World Example with Segger J-Link as Debugger

---
+++ Hello_World/build/max9867.d
@@ -0,0 +1,29 @@
+C:/MaximSDK/Examples/MAX32665/Hello_World/build/max9867.o: \
+ C:/MaximSDK/Libraries/MiscDrivers/CODEC/max9867.c \
+ c:\maximsdk\tools\gnutools\10.3\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\machine\_default_types.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\features.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\_newlib_version.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\_intsup.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\_stdint.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/i2c.h \
+ c:\maximsdk\tools\gnutools\10.3\lib\gcc\arm-none-eabi\10.3.1\include\stdbool.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/mxc_sys.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/mxc_device.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/max32665.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/core_cm4.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/cmsis_version.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/cmsis_compiler.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/cmsis_gcc.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/mpu_armv7.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/system_max32665.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/mxc_errors.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/mxc_pins.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/gpio.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/gpio_regs.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/gcr_regs.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/i2c_regs.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/dma.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/dma_regs.h \
+ C:/MaximSDK/Libraries/MiscDrivers/CODEC/max9867.h
