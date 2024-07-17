e59786dffb37d1251c208c66e02302ef1381aa08
Patrick C <patrick@chaopricha.com>
2024-07-16 21:05:34 -0400

    Commit Sleep Timer Example

---
+++ WUT/build/backup_max32665.d
@@ -0,0 +1,2 @@
+C:/Users/Patrick/Documents/GitHub/WUT/build/backup_max32665.o: \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Source/GCC/backup_max32665.S
---
+++ WUT/build/startup_max32665.d
@@ -0,0 +1,2 @@
+C:/Users/Patrick/Documents/GitHub/WUT/build/startup_max32665.o: \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Source/GCC/startup_max32665.S
