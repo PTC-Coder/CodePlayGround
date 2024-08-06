5ef2724c8d66f733eaaa966f6f9fc72ed0e9db94
Patrick <ptc48@cornell.edu>
2024-08-06 09:59:13 -0400

    Commit SPIFX to W25N02KV driver

---
+++ SPIXF/build/Ext_Flash.d
@@ -0,0 +1,11 @@
+C:/Users/ptcha/OneDrive/Documents/GitHub/CodePlayGround/SPIXF/build/Ext_Flash.o: \
+ ExtMemory/Ext_Flash.c \
+ c:\maximsdk\tools\gnutools\10.3\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\machine\_default_types.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\features.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\_newlib_version.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\_intsup.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\_stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ ExtMemory/Ext_Flash.h
---
+++ SPIXF/build/ramfunc.d
@@ -0,0 +1,22 @@
+C:/Users/ptcha/OneDrive/Documents/GitHub/CodePlayGround/SPIXF/build/ramfunc.o: \
+ ramfunc.c \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/mxc_device.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/max32665.h \
+ c:\maximsdk\tools\gnutools\10.3\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\stdint.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\machine\_default_types.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\features.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\_newlib_version.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\_intsup.h \
+ c:\maximsdk\tools\gnutools\10.3\arm-none-eabi\include\sys\_stdint.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/core_cm4.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/cmsis_version.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/cmsis_compiler.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/cmsis_gcc.h \
+ C:/MaximSDK/Libraries/CMSIS/5.9.0/Core/Include/mpu_armv7.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/system_max32665.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/mxc_errors.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/mxc_pins.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/gpio.h \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Include/gpio_regs.h \
+ C:/MaximSDK/Libraries/PeriphDrivers/Include/MAX32665/gpio.h
